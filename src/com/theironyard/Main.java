ccd25ae582c25290e3ddd09d34ad0fdd4eaef523
alhanger <hanger129@gmail.com>
2015-10-29 14:41:58 -0400

    Add initial get method and login functionality

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,5 +1,11 @@
 package com.theironyard;
 
+import spark.ModelAndView;
+import spark.Session;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
@@ -7,5 +13,41 @@ public class Main {
     public static void main(String[] args) {
 	    HashMap<String, User> users = new HashMap<>();
 
+        Spark.get(
+                "/",
+                ((request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+
+                    HashMap m = new HashMap();
+                    m.put("concerts", users.get(username).concerts);
+                    return new ModelAndView(m, "concerts.html");
+                }),
+                new MustacheTemplateEngine()
+        );
+        Spark.post(
+                "create-account",
+                ((request, response) -> {
+                    Session session = request.session();
+
+                    String username = request.queryParams("username");
+                    String password = request.queryParams("password");
+
+                    session.attribute("username", username);
+
+                    if (users.get(username) == null) {
+                        User user = new User(username, password, 0, new ArrayList<Concert>());
+                        users.put(username, user);
+                        response.redirect("/");
+                    }
+                    else if (password.equals(users.get(username).password) && username.equals(users.get(username).username)) {
+                        response.redirect("/");
+                    }
+                    else {
+                        return "There was an error";
+                    }
+                    return "";
+                })
+        );
     }
 }
--- src/com/theironyard/User.java
+++ src/com/theironyard/User.java
@@ -6,8 +6,15 @@ import java.util.ArrayList;
  * Created by alhanger on 10/29/15.
  */
 public class User {
-    String firstName;
-    String lastName;
+    String username;
+    String password;
     int concertNum;
     ArrayList<Concert> concerts;
+
+    public User(String username, String password, int concertNum, ArrayList<Concert> concerts) {
+        this.username = username;
+        this.password = password;
+        this.concertNum = concertNum;
+        this.concerts = concerts;
+    }
 }
